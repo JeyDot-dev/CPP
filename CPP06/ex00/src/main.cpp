44ab2c2f4d4b55913c483201314bc628a3572e41
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-02-25 21:25:44 +0100

    CPP06/ex00 Little fix

--- CPP06/ex00/src/main.cpp
+++ CPP06/ex00/src/main.cpp
@@ -2,7 +2,10 @@
 int main(int ac, char **av)
 {
     if (ac < 2)
+    {
         std::cout << "pls, enter numbers" << std::endl;
+        return 1;
+    }
     for (int i = 1; i < ac; i++)
     {
         std::cout << "**Converting: "<< av[i] << std::endl;
