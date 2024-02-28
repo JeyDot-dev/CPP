ff176b48d51e7e249fabc65c371da87f09aef7d6
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-02-28 18:47:27 +0100

    CPP07/ex01_00

---
+++ CPP07/ex01/inc/Tmain.hpp
@@ -0,0 +1,15 @@
+#include <cstddef>
+
+template <typename T>
+void    iter(T* a, size_t size, void (*f) (T&))
+{
+    for (size_t i = 0; i < size ; i++)
+        f(a[i]);
+}
+
+template <typename T, size_t N> //Cooler version IMO v
+void    iter2(T (&a)[N], void (*f) (T&))
+{
+    for (size_t i = 0; i < N ; i++)
+        f(a[i]);
+}
