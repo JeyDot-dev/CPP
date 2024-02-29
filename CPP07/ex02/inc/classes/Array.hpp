c903c8420ae38c0c4c41e6d2d8f9e19255855928
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-02-29 18:26:28 +0100

    CPP07_ex02_00

--- CPP07/ex02/inc/classes/Array.hpp
+++ CPP07/ex02/inc/classes/Array.hpp
@@ -1,27 +1,29 @@
 #ifndef		ARRAY_HPP
 # define	ARRAY_HPP
+
 #include <cstddef>
 #include <exception>
+#include <iostream>
+
 template <typename T>
 class	Array
 {
 
 public:
-	Array<T>(Array<T> const & src);
-	Array<T>(size_t nb_elements);
+	Array(Array<T> const & src);
+	Array(size_t nb_elements);
 
     size_t const&   size()  const;
 
-	Array<T>&	operator=(Array const& rhs);
-    T&          operator[](size_t pos);
-    T const&    operator[](size_t pos) const;
+	Array<T>&	operator=(Array<T> const& rhs);
+    T&          operator[](size_t pos) const;
 
-	Array<T>(void);
-	~Array<T>(void);
+	Array(void);
+	~Array(void);
 
 private:
-    T*              content;
-    size_t const    _size;
+    size_t  _size;
+    T*      _content;
 
     class OutOfBoundsException : public std::exception
     {
@@ -29,5 +31,83 @@ private:
     };
 
 };
+//--------------Functions----------------//
+template <typename T>
+size_t const&   Array<T>::size()    const
+{
+    return _size;
+}
+//--------------Operators----------------//
+template <typename T>
+Array<T>&	Array<T>::operator=(Array<T> const&  rhs)
+{
+	if (this != &rhs)
+	{
+        if (!this->_content || this->size() > 0)
+            delete[] this->_content;
+        this->_size = rhs.size();
+        this->_content = new T[rhs.size()];
+        for (size_t i = 0; i < rhs.size(); i++)
+            (*this)[i] = rhs[i];
+	}
+	std::cout << "Assignation operator called (array)" << std::endl;
+	return *this;
+}
+template <typename T>
+T&          Array<T>::operator[](size_t pos) const
+{
+    if (pos >= size())
+    {
+        std::cerr << "Trying to access index: " << pos << " in array of size: " << size() << std::endl;
+        throw(OutOfBoundsException());
+    }
+    return this->_content[pos];
+}
+//--------------Constructors------------//
+template <typename T>
+Array<T>::Array(Array<T> const &src) : _size(0), _content(NULL)
+{
+	*this = src;
+	std::cout << "Copy constructor called (array)" << std::endl;
+	return ;
+}
+template <typename T>
+Array<T>::Array(size_t nb_elements)
+{
+    _size = nb_elements;
+    _content = new T[_size];
+}
+template <typename T>
+Array<T>::Array(void) : _size(0), _content(NULL)
+{
+	std::cout << "Default constructor called (array)" << std::endl;
+	return ;
+}
 
+template <typename T>
+Array<T>::~Array(void)
+{
+    if (this->size() > 0)
+        delete[] this->_content;
+    std::cout << "Destructor called (array)" << std::endl;
+	return ;
+}
+template <typename T>
+char const* Array<T>::OutOfBoundsException::what() const throw()
+{
+    return "exception: You should segfault lel";
+}
+template <typename T>
+std::ostream& operator<<(std::ostream &o, Array<T> const& rhs)
+{
+    o << "[";
+    for (size_t i = 0; i < rhs.size(); i++)
+    {
+        o << rhs[i];
+        if (i != rhs.size() - 1)
+            o << ", ";
+    }
+    o << "]";
+    return o;
+}
 #endif
--- CPP07/ex02/src/main.cpp
+++ CPP07/ex02/src/main.cpp
@@ -1,6 +1,75 @@
+#include <cstdlib>
 #include <iostream>
 #include "Array.hpp"
 
-int main(void)
+#define MAX_VAL 750
+int main_subject()
 {
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    //SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete [] mirror;//
+    return 0;
+}
+
+int main(int, char**)
+{
+    std::cout << "****TEST THE MAIN FROM THE SUBJECT" << std::endl;
+    main_subject();
+    std::cout << "****MY TESTS" << std::endl;
+    Array<std::string>  str_tests(4);
+    Array<float>        wont_work(3);
+    try
+    {
+        str_tests[0] = "Wapiti";
+        str_tests[1] = "Margaritha";
+        str_tests[2] = "Perceval";
+        str_tests[3] = "Moon";
+        std::cout << str_tests << std::endl;
+//        wont_work = str_tests;
+    }
+    catch (std::exception& e)
+    {
+        std::cout << e.what() << std::endl;
+    }
 }
