a205ba718c3760278f533c8eb0284410589790cc
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2024-03-04 18:44:31 +0100

    CPP09/ex02 some progress

--- CPP09/ex02/inc/classes/PmergeMe.hpp
+++ CPP09/ex02/inc/classes/PmergeMe.hpp
@@ -1,7 +1,12 @@
 #ifndef		PMERGEME_HPP
 # define	PMERGEME_HPP
 #include <iostream>
-
+#include <time.h>
+#include <ctime>
+#include <vector>
+#include <list>
+#include <exception>
+#include <typeinfo>
 class	PmergeMe
 {
 
@@ -9,10 +14,43 @@ public:
 	PmergeMe(PmergeMe const & src);
 	PmergeMe&	operator=(PmergeMe const& rhs);
 
+    bool    sort(int ac, char** str) const;
+
 	PmergeMe(void);
 	~PmergeMe(void);
 
 private:
+    typedef std::vector<unsigned int>   pvector;
+    typedef std::list<unsigned int>     plist;
+    typedef plist::iterator             plist_it;
+
+    void    checkStr(int ac, char** av) const;
+    void    parse(int ac, char** av, pvector& vec) const;
+    void    parse(int ac, char** av, plist& list) const;
+    template <typename T>
+    void    mergeInsertSort(T& cont, size_t l, size_t r) const;
+    void    mergeSort(pvector& vec, size_t l, size_t m, size_t r) const;
+    void    mergeSort(plist& list, size_t l, size_t m, size_t r) const;
+    void    insertSort(pvector& vec, size_t l, size_t r) const;
+    void    insertSort(plist& list, size_t l, size_t r) const;
+    void    printResults(time_t timer[2][2], pvector const& vec, plist const& list) const;
+
+    class   BadFormat : public std::exception
+    {
+        char const* what() const throw() {return "Something wrong with the input";}
+    };
+    class   DupInput : public std::exception
+    {
+        char const* what() const throw() {return "Duplicate string in input";}
+    };
+    class   EmptyString : public std::exception
+    {
+        char const* what() const throw() {return "Empty string in input";}
+    };
+    class   NegativeNum : public std::exception
+    {
+        char const* what() const throw() {return "One or multiple numbers are negative.";}
+    };
 
 };
 
--- CPP09/ex02/src/classes/PmergeMe.cpp
+++ CPP09/ex02/src/classes/PmergeMe.cpp
@@ -1,33 +1,102 @@
 #include "PmergeMe.hpp"
 
 //--------------Functions----------------//
+void    PmergeMe::insertSort(pvector& vec, size_t l, size_t r) const
+{
+}
+void    PmergeMe::insertSort(plist& list, size_t l, size_t r) const
+{
+    if (list.size() / 2 >= l)
+    {
+        plist_it    it = list.begin();
+        for(size_t i = 0; i < l; i++)
+            it++;
+    }
+    else
+    {
+        plist_it    it_end = list.end();
+        it_end--;
+        for (size_t i = 0; i < list.size() - l; i++)
+            it_end++;
+    }
+
+    
+}
+template <typename T>
+void    PmergeMe::mergeInsertSort(T& cont, size_t l, size_t r) const
+{
+    if (cont.size() <= 1)
+        return;
+    if ((cont.size() <= 32 && typeid(T) == typeid(pvector))
+            || (cont.size() <= 12 && typeid(T) == typeid(plist)))
+        insertSort(cont);
+    else
+    {
+        size_t  m = (l - r) / 2 + l;
+        mergeInsertSort(cont, l, m);
+        mergeInsertSort(cont, m + 1, r);
+        mergeSort(cont, l, m, r);
+    }
+}
+void    PmergeMe::parse(int ac, char** av, pvector& vec) const
+{
+    (void)ac;
+    (void)av;
+    (void)vec;
+}
+void    PmergeMe::parse(int ac, char** av, plist& list) const
+{
+    (void)ac;
+    (void)av;
+    (void)list;
+}
+void    PmergeMe::checkStr(int ac, char** av) const
+{
+    (void)ac;
+    (void)av;
+}
+bool    PmergeMe::sort(int ac, char** av) const
+{
+    try{checkStr(ac, av);}
+    catch(std::exception& e){
+        std::cout << e.what() << std::endl;
+        return false;}
+    time_t  timer[2][2];
+
+    time(&(timer[0][0]));
+    pvector vec;
+    parse(ac, av, vec);
+    mergeInsertSort(vec);
+    time(&(timer[0][1]));
+
+    time(&(timer[1][0]));
+    plist   list;
+    parse(ac, av, list);
+    mergeInsertSort(list);
+    time(&(timer[1][1]));
+
+    printResults(timer, vec, list);
+    return true;
+}
 //--------------Operators----------------//
 PmergeMe&	PmergeMe::operator=(PmergeMe const&  rhs)
 {
 	if (this != &rhs)
 	{
-		//copy
 	}
-	std::cout << "Assignation operator called" << std::endl;
 	return *this;
 }
 //--------------Constructors------------//
 PmergeMe::PmergeMe(PmergeMe const &src)
 {
-	*this = src;
-	std::cout << "Copy constructor called" << std::endl;
-	return ;
+    (void)src;
 }
 
 PmergeMe::PmergeMe(void)
 {
-	std::cout << "Default constructor called" << std::endl;
-	return ;
 }
 
 PmergeMe::~PmergeMe(void)
 {
-	std::cout << "Destructor called" << std::endl;
-	return ;
 }
 //--------------Non-Member--------------//
